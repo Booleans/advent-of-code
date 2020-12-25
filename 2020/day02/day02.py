2e78d540e44d8241e102c4a0f846be79932fc127
Andrew Nicholls <andrew.s.nicholls@gmail.com>
2020-12-25 09:47:53 -0500

    Updated Day02
    Deleted Jupyter notebook and moved code into .py file.

---
+++ 2020/day02/day02.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+with open('input.txt') as f:
+    inputs = [line.strip().split() for line in f]
+
+valid_passwords = 0
+
+for line in inputs:
+    low, high = line[0].split('-')
+    low = int(low)
+    high = int(high)
+    char = line[1][0]
+    password = line[2]
+
+    if low <= Counter(password)[char] <= high:
+        valid_passwords += 1
+
+print(valid_passwords)
+
+valid_passwords = 0
+
+for line in inputs:
+    low, high = line[0].split('-')
+    low = int(low)
+    high = int(high)
+    char = line[1][0]
+    password = line[2]
+
+    if (password[low - 1] == char) != (password[high - 1] == char):
+        valid_passwords += 1
+
+print(valid_passwords)

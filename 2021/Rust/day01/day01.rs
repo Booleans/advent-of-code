e9566c6eed81f5067558682faf3e0f97849fd31f
Andrew Nicholls <andrew.s.nicholls@gmail.com>
2022-05-28 06:00:53 -0700

    Solved Day 06

--- 2021/Rust/day01/day01.rs
+++ 2021/Rust/day01/day01.rs
@@ -2,6 +2,6 @@ use std::fs;
 
 fn main() {
     let filename = "../../inputs/day01.txt";
-    let contents = fs::read_to_string(filename);
+    let contents = fs::read_to_string(filename).split(r"\n");
     println!("{:?}", contents);
 }

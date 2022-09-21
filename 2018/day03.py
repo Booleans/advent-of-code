8a00e3e04673f77fa8e4f0d48c6075f09a43d203
Andrew Nicholls <andrew.s.nicholls@gmail.com>
2022-09-21 09:53:51 -0700

    Finish 2018 Day 03

--- 2018/day03.py
+++ 2018/day03.py
@@ -12,6 +12,7 @@ class Rectangle:
     x_max: int = field(init=False)
     y_min: int = field(init=False)
     y_max: int = field(init=False)
+    coordinates: set((int, int)) = field(init=False)
     
     def __post_init__(self):
         claim_components = self.claim.split()
@@ -26,6 +27,8 @@ class Rectangle:
         self.x_max = self.x_min + self.width
         self.y_min = self.inches_from_top
         self.y_max = self.y_min + self.height
+        self.coordinates = {(x, y) for x in range(self.x_min, self.x_max)
+                                   for y in range(self.y_min, self.y_max)}
 
 with open('inputs/day03.txt') as f:
     rectangles = [Rectangle(line) for line in f.read().split('\n')]
@@ -34,13 +37,22 @@ coordinates_occupied = set()
 overlapping_coordinates = set()
 
 for rectangle in rectangles:
-    for x in range(rectangle.x_min, rectangle.x_max):
-        for y in range(rectangle.y_min, rectangle.y_max):
-            coordinate = (x, y)
-            if coordinate in coordinates_occupied:
-                overlapping_coordinates.add(coordinate)
-            else:
-                coordinates_occupied.add(coordinate)
+    for coordinate in rectangle.coordinates:
+        if coordinate in coordinates_occupied:    
+            overlapping_coordinates.add(coordinate)
+        else:
+            coordinates_occupied.add(coordinate)
 
 # Part 1 solution.
 print(f'There are {len(overlapping_coordinates)} square inches that have overlapping claims.')
+
+rectangle_with_no_overlap = None
+
+for i, rectangle1 in enumerate(rectangles):
+    all_other_rectangles = rectangles[:i] + rectangles[i+1:]
+    if not any(set.intersection(rectangle1.coordinates, rectangle2.coordinates) for rectangle2 in all_other_rectangles):
+        rectangle_with_no_overlap = rectangle1
+        break
+
+# Part 2 solution.
+print(f'The ID of the only claim that doesn\'t overlap is {rectangle_with_no_overlap.ID}.')

c7d98f794e689b2562996ae0b802c4f59331ec6a
Andrew Nicholls <andrew.s.nicholls@gmail.com>
2022-10-01 19:33:57 -0700

    Update day04.py
    Applied Python Black formatting.

--- 2020/day04/day04.py
+++ 2020/day04/day04.py
@@ -1,33 +1,39 @@
 import re
 
-with open('input.txt') as f:
+with open("input.txt") as f:
     raw = f.read()
 
 # Passports are separated by '\n\n'.
-passports = [passport.replace('\n', ' ') for passport in raw.split('\n\n')]
+passports = [passport.replace("\n", " ") for passport in raw.split("\n\n")]
+
 
 def all_fields_present(passport):
-    pattern = '^.*(?=.*byr)(?=.*iyr)(?=.*eyr)(?=.*hgt)(?=.*hcl)(?=.*ecl)(?=.*pid).*$'
+    pattern = "^.*(?=.*byr)(?=.*iyr)(?=.*eyr)(?=.*hgt)(?=.*hcl)(?=.*ecl)(?=.*pid).*$"
     valid = re.compile(pattern)
     return bool(valid.match(pattern, passport))
+
 
 # Part 1 solution, a passport is valid if all required fields are present.
 print(sum(all_fields_present(passport) for passport in passports))
 
+
 def all_fields_present_and_valid(passport):
     fields = {
-        'byr':r'(?=.*byr:(19[2-9][0-9]|200[0-2])\b)',
-        'iyr':r'(?=.*iyr:(201[0-9]|2020)\b)',
-        'eyr':r'(?=.*eyr:(202[0-9]|2030)\b)',
-        'hgt':r'(?=.*hgt:((59|6\d|7[0-6])in|(1[5-8]\d|19[0-3])cm)\b)',
-        'hcl':r'(?=.*hcl:#[\d|a-f]{6}\b)',
-        'ecl':r'(?=.*ecl:(amb|blu|brn|gry|grn|hzl|oth)\b)',
-        'pid':r'(?=.*pid:\d{9}\b)'  
+        "byr": r"(?=.*byr:(19[2-9][0-9]|200[0-2])\b)",
+        "iyr": r"(?=.*iyr:(201[0-9]|2020)\b)",
+        "eyr": r"(?=.*eyr:(202[0-9]|2030)\b)",
+        "hgt": r"(?=.*hgt:((59|6\d|7[0-6])in|(1[5-8]\d|19[0-3])cm)\b)",
+        "hcl": r"(?=.*hcl:#[\d|a-f]{6}\b)",
+        "ecl": r"(?=.*ecl:(amb|blu|brn|gry|grn|hzl|oth)\b)",
+        "pid": r"(?=.*pid:\d{9}\b)",
     }
 
-    valid_pattern = '^' + ''.join([pattern for field, pattern in fields.items()]) + '.*$'
+    valid_pattern = (
+        "^" + "".join([pattern for field, pattern in fields.items()]) + ".*$"
+    )
     valid_passport = re.compile(valid_pattern)
     return bool(valid_passport.match(passport))
+
 
 # Part 2 solution, a passport is valid if all required fields are present and have a valid input.
 print(sum(all_fields_present_and_valid(passport) for passport in passports))

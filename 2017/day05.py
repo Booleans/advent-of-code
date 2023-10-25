dc20eef5cf81032c4f3b141737f77bf2c54439ad
Andrew Nicholls <andrew.s.nicholls@gmail.com>
2023-10-24 18:54:31 -0700

    Solved 2017 Day 05 Part 2

--- 2017/day05.py
+++ 2017/day05.py
@@ -1,12 +1,31 @@
 with open("inputs/day05.txt") as f:
-    jumps = [int(digit) for digit in f.read().split("\n")]
+    JUMPS = [int(digit) for digit in f.read().split("\n")]
 
 steps_taken = 0
 i = 0
 
+jumps = [num for num in JUMPS]
+
 while i in range(len(jumps)):
     jump = jumps[i]
     jumps[i] += 1
+    i += jump
+    steps_taken += 1
+else:
+    print(steps_taken)
+
+
+steps_taken = 0
+i = 0
+
+jumps = [num for num in JUMPS]
+
+while i in range(len(jumps)):
+    jump = jumps[i]
+    if jump >= 3:
+        jumps[i] -= 1
+    else:
+        jumps[i] += 1
     i += jump
     steps_taken += 1
 else:
